adb4355cab69600ea147e00d27675c37670b67fe
Aayush <Aayushstha25@gmail.com>
2022-03-24 22:43:22 +0545

    Added a dashboard and route for the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/dashboard', to: 'users#dashboard'
+  post '/edit', to: 'users#update'
+
   root to: "pages#home"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
